4c264b07a0c4b67539dbe7f8696e7a813ab745de
Mostafa Maki <mostafa_m9@yahoo.com>
2022-10-11 21:15:55 -0700

    removed old code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,3 @@
 const router = require('express').Router();
-const { BlogUser, BlogPost, BlogComment } = require('../models');
+const { } = require('../models');
 const withAuth = require('../utils/authorization');
