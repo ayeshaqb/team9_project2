e9d838ebad05fd477f61cc0c06da05391765b591
Mostafa Maki <mostafa_m9@yahoo.com>
2022-10-12 21:38:47 -0700

    created home route with mock up home page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
-const { } = require('../models');
-const withAuth = require('../utils/authorization');
+// const { } = require('../models');
+// const withAuth = require('../utils/authorization');
+
+router.get('/home', async (req, res) => {
+    res.render('home')
+});
+
+module.exports = router;
