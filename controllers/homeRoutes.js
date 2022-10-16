75a23eb4f8285bc518104b4f6fdf299aabc96ad2
Mostafa Maki <mostafa_m9@yahoo.com>
2022-10-15 18:19:29 -0700

    added user and character models with example seeds

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
-// const { } = require('../models');
+const { User, Character } = require('../models');
 // const withAuth = require('../utils/authorization');
 
 router.get('/home', async (req, res) => {
-    res.render('home')
+    try {
+        const userData = await User.findAll({
+            include: [{ model: Character}],
+        }) 
+
+        const cleanData = userData.map((data) => data.get({ plain: true }));
+        console.log(cleanData)
+        res.render('home', {
+            cleanData
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/visit', async (req, res) => {
