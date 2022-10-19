4c0ce840725b622c152cb8f319751fcd9dad4767
Mostafa Maki <mostafa_m9@yahoo.com>
2022-10-18 17:25:29 -0700

    updated the models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,13 @@
 const User = require('./User');
-const Character = require('./character');
+const Character = require('./Character');
+const CharUser = require('./CharUser')
 
 User.hasMany(Character, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(CharUser, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
@@ -10,4 +16,18 @@ Character.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Character };
+// might need to be adjusted
+Character.hasMany(CharUser, {
+  foreignKey: 'character_id',
+  onDelete: 'CASCADE'
+});
+
+CharUser.belongsTo(Character, {
+  foreignKey: 'character_id',  
+});
+
+CharUser.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Character, CharUser };
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Character } = require("../models");
+const { User, Character, CharUser } = require("../models");
 
 const userData = require("./userData.json");
 const characterData = require("./characterData.json");
+const charUserData = require("./characterUser.json")
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -17,6 +18,9 @@ const seedDatabase = async () => {
           user_id: users[Math.floor(Math.random() * users.length)].id,
         });
     }
+
+    const charUsersData = await CharUser.bulkCreate(charUserData);
+
     process.exit(0);
 };
 
